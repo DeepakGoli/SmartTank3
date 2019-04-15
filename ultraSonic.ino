c00e9d68f026fd808c5161bb6a5a2d86127a56a4
DeepakGoli <45971013+DeepakGoli@users.noreply.github.com>
2019-04-15 16:25:14 +0530

    Add files via upload

---
+++ ultraSonic.ino
@@ -0,0 +1,21 @@
+const int trigPin = 9;
+const int echoPin = 10;
+long duration;
+double distance;
+
+void setup(){
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  Serial.begin(9600);
+}
+ void loop(){
+  digitalWrite(trigPin ,LOW);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  duration = pulseIn(echoPin,HIGH);
+  distance = duration*0.034/2;
+  Serial.print("Distance(in cm): ");
+  Serial.println(distance);
+  }
+
